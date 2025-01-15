f524081dd1eb00a93cc1004d00feac937fed417b
PINIX1101 <fajartrisnopurwanto@yahoo.co.id>
2025-01-15 11:14:02 +0700

    Add Mandatory Field for Session

--- fajaracademy/models/fajaracademy_session.py
+++ fajaracademy/models/fajaracademy_session.py
@@ -10,5 +10,5 @@ class FajaracademySession(models.Model):
     duration = fields.Float('Duration')
     number_of_seats = fields.Float('Number of Seats')
     instructor_id = fields.Many2one('res.partner', string='Instructor')
-    course_id = fields.Many2one('fajaracademy.course', string='Course')
+    course_id = fields.Many2one('fajaracademy.course', string='Course', required=True)
     attendees_ids = fields.Many2many('res.partner', string='Attendees')
